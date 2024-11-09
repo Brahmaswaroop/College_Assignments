641be6dc292d23e34f1dbaf3694f2bc6761f459a
Brahmaswaroop <144714539+Brahmaswaroop@users.noreply.github.com>
2024-11-09 09:56:42 +0530

    added comments

--- grade_calculator.py
+++ grade_calculator.py
@@ -2,7 +2,9 @@ def mark_to_grade(sml, n):
     mak_grad = {90: "A+", 80: "A", 70: "B+", 60: "B", 50: "C+", 40: "C", 30: "P", 0: "F"}
     sub_grad_list, total_marks = {}, 0
     for x, y in sml.items():
-        total_marks += y
+        total_marks += y    # sum of all marks
+        # creating the grade list by look in the mak_grad 
+        # y - (y%10) is to make the persentage multiples of ten
         sub_grad_list[x] = mak_grad[y - (y % 10) if y > 30 else 0]
     total_grade = mak_grad[total_marks/n - (total_marks/n % 10) if total_marks > 30 else 0]
     return sub_grad_list, total_grade
@@ -16,9 +18,11 @@ try:
         subj_list[subj] = mark
 
     subj_grades, grade = mark_to_grade(subj_list, num)
+
     print("Grade in subject:\n")
     for i, j in subj_grades.items():
         print(f"{i} = {j}")
+
     print(f"Total grade: {grade}")
 except ValueError:
     print("Enter a valid response.")
