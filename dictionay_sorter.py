3355eed7c220de092037a3c5f08ebfe8938c0bd7
Brahmaswaroop <144714539+Brahmaswaroop@users.noreply.github.com>
2024-11-09 09:08:58 +0530

    Update dictionay_sorter.py

--- dictionay_sorter.py
+++ dictionay_sorter.py
@@ -1,16 +1,13 @@
 from ast import literal_eval
 
 def dict_sort(dict1):
-    temp = literal_eval(dict1)
-    temp = list(dict1)
-    temp.sort()
-    sorted_dict = {i: dict1[i] for i in temp}
+    sorted_dict = {i: dict1[i] for i in sorted(dict1)}
     return sorted_dict
 
 try:
     x = input("Enter the entire dictionary with braces:\n")
     in_dict = literal_eval(x)
-    print(dict_sort(x))
+    print(dict_sort(in_dict))
 except ValueError:
     print("Enter a valid response.")
     print("Eg:  {'name': 'John'}")
